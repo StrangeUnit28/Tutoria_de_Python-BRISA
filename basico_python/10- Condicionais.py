70d500afd2e0c9fa09738e581e30682147959a1d
Eduardo Gurgel <51385738+EduardoGurgel@users.noreply.github.com>
2023-05-30 15:46:32 -0300

    Add 10 - Condicionais.py
    Adicionando o arquivo com as condicionais e exemplo em python

---
+++ basico_python/10- Condicionais.py
@@ -0,0 +1,23 @@
+### Estrutura condicional if
+if condição:
+    # Aqui a parte do código que será executada caso a condição for verdadeira
+
+### Estrutura condicional elif
+elif condição:
+    # Aqui a parte do código que será executada caso a condição anterior for falsa e essa condição for verdadeira
+
+### Estrutura condicional else
+else:
+    # Aqui a parte do código que será executada caso todas as outras condições forem falsas
+
+### Exemplos
+
+valor = 18
+
+if idade < 18:
+    print("Você é menor de idade.")
+elif idade == 18:
+    print("Você acabou de completar 18 anos.")
+else:
+    print("Você é maior de idade.")
+
