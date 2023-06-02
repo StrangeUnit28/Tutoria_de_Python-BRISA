2e909c7eabeea7c90cd8ccec4ed1659be0c8281e
leonardomilv3 <leonardomilov@gmail.com>
2023-06-02 09:57:37 -0300

    :memo: adicionando info data frames

--- basico_pandas/2-info-data-frame.py
+++ basico_pandas/2-info-data-frame.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 """
     A biblioteca pandas é uma das principais bibliotecas do Python para manipulação 
-    e análise de dados. Nesse veremos as informações de um Data Frame criado
+    e análise de dados. Nesse arquivo veremos as informações de um Data Frame criado
 """
 
 # Verifica o tipo de objeto criado
@@ -12,17 +12,23 @@ print(type(df))
 
 # Identifica quantas linhas e colunas do Data Frame
 print("Linhas e Colunas Data Frame")
-df.shape
+print(df.shape)
 
 # Verifica as formato se encontram os dados em cada coluna, 
 # além da quantidade de memória para ler esse conjunto de dados do Data Frame
 print("Informações sobre Data Frame")
 print(df.info())
 
-# Visualizando as primeiras n linhas do Data Frame
-print("As primeiras n linhas do Data Frame")
-df.head(n=6)
+# Visualiza a estatística dos dados do Data Frame
+print(df.describe())
 
-# Visualizando as últimas n linhas do Data Frame
+# Visualiza as colunas do Data Frame
+print(df.columns)
+
+# Visualiza as primeiras n linhas do Data Frame
+print("As primeiras n linhas do Data Frame")
+print(df.head(n=6))
+
+# Visualiza as últimas n linhas do Data Frame
 print("As últimas n linhas do Data Frame")
-df.tail(n=6)
+print(df.tail(n=6))
