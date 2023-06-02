5f4d1b4ad1c9511d641fe7377232e22e935ca2bc
VitorB2002 <vitorb.santos2002@gmail.com>
2023-06-02 00:23:51 -0300

    Add: adicionado calculo do valor máx, min e média da coluna valor transferido

--- exercicios/exercicio1.py
+++ exercicios/exercicio1.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('transferencias - 05_2023.csv')
 
 df = df.drop(columns=["ANO / MÊS"])
 df['VALOR TRANSFERIDO'] = df['VALOR TRANSFERIDO'].str.replace(',', '.')
 df["VALOR TRANSFERIDO"]= df["VALOR TRANSFERIDO"].astype('float')
+valor_min = np.min(df['VALOR TRANSFERIDO'])
+valor_max = np.max(df['VALOR TRANSFERIDO'])
+media = np.mean(df['VALOR TRANSFERIDO'])
 print(df)
+print(
+    "Valor minímo: " + f'{valor_min}' +
+    "\nValor máximo: " + f'{valor_max}' +
+    "\nMédia: " + f'{media}')
