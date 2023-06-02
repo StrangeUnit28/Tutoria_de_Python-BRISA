27861972de6b0cc8fa944b1af7917091067624a4
VitorB2002 <vitorb.santos2002@gmail.com>
2023-06-02 13:54:54 -0300

    Add: adicionado módulo de funções básicas para matematica

---
+++ basico_python/Modulos/matemagica.py
@@ -0,0 +1,16 @@
+"""Módulo criado para funções básicas da mátematica"""
+def soma(x, y):
+    """Soma dois números"""
+    return x + y
+
+def sub(x, y):
+    """Subtrai dois números"""
+    return x - y
+
+def div(x, y):
+    """Divide dois números"""
+    return x / y
+
+def mul(x, y):
+    """Multiplica dois números"""
+    return x * y
