b1d194addf313ba12e6d7bb4aae3555afebbdae3
leonardomilv3 <leonardomilov@gmail.com>
2023-06-02 16:02:16 -0300

    :memo: adicionando acesso colunas df

--- basico_pandas/2-info-data-frame.py
+++ basico_pandas/2-info-data-frame.py
@@ -7,22 +7,28 @@ import pandas as pd
 
 # Verifica o tipo de objeto criado
 df = pd.read_csv('../exercicios/transferencias - 05_2023.csv')
-print("Tipo objeto df")
+print("Tipo objeto df: ", end="")
 print(type(df))
 
 # Identifica quantas linhas e colunas do Data Frame
-print("Linhas e Colunas Data Frame")
+print("Linhas e Colunas (shape) Data Frame: ")
 print(df.shape)
 
 # Verifica as formato se encontram os dados em cada coluna, 
 # além da quantidade de memória para ler esse conjunto de dados do Data Frame
-print("Informações sobre Data Frame")
+print("Informações sobre Data Frame:")
 print(df.info())
 
 # Visualiza a estatística dos dados do Data Frame
+print("Descrição e estatísticas df: ")
 print(df.describe())
 
+# Visualiza a quantidade de memória usada por cada coluna (em bytes)
+print("Uso de memória: ")
+print(df.memory_usage(deep=True))
+
 # Visualiza as colunas do Data Frame
+print("Colunas df: " , end="")
 print(df.columns)
 
 # Visualiza as primeiras n linhas do Data Frame
